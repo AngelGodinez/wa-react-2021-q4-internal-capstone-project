482f99a66083789f90579656f591975bec13cc6c
Angel Godinez <jose.pena@wizeline.com>
2021-11-14 22:17:13 -0500

    added icons and first header

--- src/header.component.jsx
+++ src/header.component.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
+import emptycart from './assets/empty-shopping-cart.png';
 
 const HeaderWrapper = styled.div`
   overflow: hidden;
-  background-color: #333;
+  background-color: #5a5a5a;
   position: fixed;
   top: 0;
   width: 100%;
   height: 50px;
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: space-between;
   color: white;
   align-items: center;
+  img {
+    max-height: 40px;
+    padding-right: 20px;
+  }
+  .brand__name {
+    padding-left: 8px;
+    font-size: 20px;
+  }
 `;
 
 function HeaderComponent() {
   return (
-    <HeaderWrapper>
-      texto
-      texto
-      texto
-      texto
+    <HeaderWrapper>      
+      <span className="brand__name">
+        THE SHOP
+      </span>
+      <div>
+        <input type="text" />
+        <button type="button">search</button>
+      </div>
+      <img src={emptycart} alt="cart"/>
     </HeaderWrapper>
   )
 }
--- src/slider.component.jsx
+++ src/slider.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import featured_banners from './featured-banners';
 
 function SliderComponent() {
+  console.log(featured_banners);
   return (
     <div>
       Slider component
