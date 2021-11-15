a9cb6b9e6063dec2bd2a761b2d7657aa40cfd737
Angel Godinez <jose.pena@wizeline.com>
2021-11-14 23:11:54 -0500

    slider working

--- src/homePage.jsx
+++ src/homePage.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import SliderComponent from './slider.component';
 import CarouselComponent from './carousel.component';
 import GridComponent from './grid.component';
+import styled from 'styled-components';
+import featured_banners from './featured-banners';
 
+const { results } = featured_banners;
+
+const HomeWrapper = styled.div`
+  width: 95%;
+`;
 function HomePage() {
   return (
-    <div>
-      <SliderComponent/>
+    <HomeWrapper>
+      <SliderComponent slides={results}/>
       <CarouselComponent/>
       <GridComponent/>
-    </div>
+    </HomeWrapper>
   )
 }
 
