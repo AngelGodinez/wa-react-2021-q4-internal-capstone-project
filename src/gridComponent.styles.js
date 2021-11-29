4d2663fb745ab93df76d9ac506a498552b7ab232
Angel Godinez <jose.pena@wizeline.com>
2021-11-29 11:47:09 -0500

    media query added

--- src/gridComponent.styles.js
+++ src/gridComponent.styles.js
@@ -1,10 +1,26 @@
 import styled from "styled-components";
+import { device } from "./device";
 
 const GridWrapper = styled.div`
   align-self: center;
   width: 80%;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  
+  @media ${device.mobileM} {
+    grid-template-columns: 1fr;
+  }
+  @media ${device.mobileL} { 
+    grid-template-columns: 1fr 1fr;
+  }
+  @media ${device.laptop} { 
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+  @media ${device.laptopL} { 
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+  @media ${device.desktop} { 
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  }
   grid-template-rows: auto;
   gap: 15px 10px;
 `;
